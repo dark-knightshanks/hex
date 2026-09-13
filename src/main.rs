f6fd7e188ae44c3c5dcbfdb5d1149efa4b000808
knight <rishirajrajgor@gmail.com>
2026-09-13 17:43:44 +0530

    added more basic commands(cd, pwd, exit)

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,47 @@ fn main() {
                 if trimmed.is_empty(){
                     continue;
                 }
-                println!("Line: {}", trimmed);
+                let parts: Vec<&str> = trimmed.split_whitespace().collect(); 
+                let command = parts[0];
+                let args = &parts[1..];
+                match command{
+                "exit" => {
+                    println!(">>Bye-Bye, See you soon!!");
+                    break
+                    },
+                "pwd" =>{
+                    match std::env::current_dir(){
+                        Ok(path) => {
+                            println!("path: {}", path.display());
+                            continue
+                            },
+                        Err(e) => {
+                            println!("pwd error {}", e);
+                            continue
+                            }   
+                        };
+                    },
+                "cd" => {
+                    let target = match args.get(0) {
+                        Some(path_arg) => path_arg.to_string(),
+                        None => match std::env::var("HOME") {
+                            Ok(home) => home,
+                            Err(_) => {
+                                println!("cd: HOME directory not set");
+                                continue;
+                            }
+                        },
+                    };
+
+                    if let Err(e) = std::env::set_current_dir(&target) {
+                        println!("cd error {}", e);
+                    }
+                },
+                _ => {
+                    println!("Unknown command {}", command);
+                    continue
+                    }
+                }
             },
             Err(ReadlineError::Interrupted) => {
                 println!("^C");
